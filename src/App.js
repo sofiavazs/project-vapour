5b56b7e7c1903d1f9b1140bac7f9e7ad9a7a4e57
Sofia Sousa <sofiavazsousa@gmail.com>
2020-12-09 23:26:49 +0100

    Game page now functional + basic styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import { GamePage } from 'components/GamePage';
 import { GamesLibrary } from 'components/GamesLibrary';
 import { Header } from 'components/Header';
 import { FavouriteGames } from 'components/FavouriteGames'
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <FavouriteGames />
       <Switch>
         <Route path="/" exact>
+          <FavouriteGames />
           <GamesLibrary />
         </Route>
         <Route path="/games/:slug" exact>
--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -2,27 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export const GamePage = () => {
-  const { gameSlug } = useParams();
-  const [game, setGame] = useState({});
+  const { slug } = useParams();
+  const [game, setGame] = useState([]);
 
   useEffect(() => {
-    const GAME_URL = `https://express-vapour.herokuapp.com/games/${gameSlug}`;
+    const GAME_URL = `https://express-vapour.herokuapp.com/games/${slug}`;
     fetch(GAME_URL)
       .then((response) => response.json())
       .then((json) => {
-        console.log(json)
         setGame(json)
+        console.log(json)
       })
       .catch((error) => {
         console.error('Request Failed', error)
       })
-  }, [gameSlug]);
+  }, [slug]);
 
   return (
     <>
       <section className="game-page">
         <div className="details-container">
           <h1>{game.name}</h1>
+          <img className="game-screenshot" src={game.background_image} alt={game.name} />
+          <p className="release-date">{`Release date: ${game.released}`}</p>
+          <p className="game-rating">{`Community rating: ${game.community_rating} /10`}</p>
         </div>
       </section>
     </>
