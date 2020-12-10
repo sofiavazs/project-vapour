d3e0988f2876d1d4644e2ea5a3b6db07734638a8
Sofia Sousa <sofiavazsousa@gmail.com>
2020-12-10 21:48:25 +0100

    Not found page styling

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Ghosts from '../assets/ghosts.gif';
+import Ghosts from '../assets/ghosts.gif'
 
 export const NotFound = () => {
   return (
     <div className="not-found">
-      <h1>Hmmm, interesting! I do not own that game! </h1>
+      <h1>Hmmm... interesting! I do not own that game!<span role="img" aria-label="ups emoji"> 🧐 </span></h1>
       <img className="ghosts" src={Ghosts} alt="pacman ghosts" />
-      <Link className="back-link" to="/">
-        <h2> BACK TO GAMES LIBRARY </h2>
+      <Link to="/">
+        <h2 className="back-link">
+         Back to Games Library
+        </h2>
       </Link>
     </div>
   );
